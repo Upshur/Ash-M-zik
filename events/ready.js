bd717587ec7b70e7c663843b4cc0060526c89aab
Glitch (glib-summer-elm) <none>
2022-01-10 22:07:24 +0000

    🎒💶 Checkpoint ./config.json:26755948/31 ./events/ready.js:26755948/20

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ const log = message => {
 
 module.exports = async client => {
   
-client.user.setActivity(`Azexa Was Here..!`, {
+client.user.setActivity(`ae!`, {
 type: "PLAYING",
 url: "https://www.twitch.tv/klanter"})
     log(`[BOT] Aktif, Komutlar Yüklendi.`)
